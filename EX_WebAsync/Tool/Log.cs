f2c1d7d6ed92ae5b969c2c54fb325e647eb424a9
zj <zj_rest@126.com>
2017-09-27 14:02:56 +0800

    简单异步读写
    异步读写、日志生成、html+一般处理程序

---
+++ EX_WebAsync/Tool/Log.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Configuration;
+using System.IO;
+using System.Text;
+using EX_WebAsync.Tool;
+
+/**
+ * 用于日志的读写
+ * 赵进
+ * 2017-09-27
+ * */
+namespace EX_WebAsync.Tool
+{
+    /// <summary>
+    /// 日志类，用于日志操作
+    /// </summary>
+    public class Log
+    {
+        //获取日志文件的地址
+        private static string LogPath = ConfigurationManager.AppSettings["LogPath"].ToString() + "Log-" + DateTime.Now.ToString("yyyy-MM-dd")+".txt";
+        
+        /// <summary>
+        /// 写入日志文件
+        /// </summary>
+        /// <param name="type">日志类型</param>
+        /// <param name="msg">日志内容</param>
+        public static void WriteLog(LogType type, string msg)
+        {
+            if (!File.Exists(LogPath))
+            {
+                File.Create(LogPath).Close();
+            }
+            StringBuilder sb = new StringBuilder();
+            sb.Append(type.ToString()+"     ");
+            sb.Append(DateTime.Now.ToString("hh:mm:ss")+"       ");
+            sb.Append(msg+@"\r\h");
+            FileStreamOperate operate = new FileStreamOperate();
+            operate.Write(LogPath, sb.ToString());
+        }
+    }
+
+    /// <summary>
+    /// 日志类型 Trace,Error,Warning,SQL
+    /// </summary>
+    public enum LogType
+    {
+        Trace,
+        Warning,
+        Error,
+        SQL
+    }
+}
---
+++ EX_WebAsync/ashxs/Index.ashx.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using EX_WebAsync.Tool;
+using System.Web.Script.Serialization;
+
+namespace EX_WebAsync.ashxs
+{
+    /// <summary>
+    /// Index 的摘要说明
+    /// </summary>
+    public class Index : IHttpHandler
+    {
+
+        public void ProcessRequest(HttpContext context)
+        {
+            string type = getData(context, "type");
+            switch (type)
+            {
+                case "read":
+                    string rfilepath = getData(context, "filepath");
+                    Read(context,rfilepath);
+                    break;
+                case "write":
+                    string wfilepath = getData(context, "filepath");
+                    string wdata = getData(context, "data");
+                    Write(context, wfilepath, wdata);
+                    break;
+            }
+        }
+
+        /// <summary>
+        /// 写入文件
+        /// </summary>
+        /// <param name="context"></param>
+        /// <param name="filepath"></param>
+        /// <param name="data"></param>
+        private void Write(HttpContext context, string filepath, string data)
+        {
+            try
+            {
+                FileStreamOperate writeoperate = new FileStreamOperate();
+                writeoperate.Write(filepath, data);
+                var msg = new { result = "success" };
+                context.Response.Write(ObjectToJSON(msg));
+            }
+            catch(Exception ex)
+            {
+                Log.WriteLog(LogType.Error, "写入文件出错，原因：" + ex.ToString());
+                var msg = new { result = "fail" };
+                context.Response.Write(ObjectToJSON(msg));
+            }
+        }
+
+        private void Read(HttpContext context,string filepath)
+        {
+            try
+            {
+                FileStreamOperate readOperate = new FileStreamOperate();
+                readOperate.Read(filepath);
+                var msg = new { result = "success", version = readOperate.DataForRead.ToString() };
+                context.Response.Write(ObjectToJSON(msg));
+            }
+            catch(Exception ex)
+            {
+                Log.WriteLog(LogType.Error, "读取文件出错，原因：" + ex.ToString());
+                var msg = new { result = "fail" };
+                context.Response.Write(ObjectToJSON(msg));
+            }
+        }
+
+        private string ObjectToJSON(object data)
+        {
+            JavaScriptSerializer serializer = new JavaScriptSerializer();
+            string result = serializer.Serialize(data);
+            return result;
+        }
+
+        private string getData(HttpContext context,string key)
+        {
+            string data = string.Empty;
+            try
+            {
+                if (context.Request[key] != null && !string.IsNullOrEmpty(context.Request[key].ToString()))
+                {
+                    data = context.Request[key].ToString();
+                }
+            }
+            catch { data = ""; }
+
+            return data;
+        }
+
+        public bool IsReusable
+        {
+            get
+            {
+                return false;
+            }
+        }
+    }
+}
